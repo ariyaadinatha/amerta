e069e8ef00de9b06cb0775aa30e3f8f176ca8592
Ariya Adinatha <ariyaadinatha21@gmail.com>
2023-05-02 16:31:52 +0700

    feat: added enemy damage and movement

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float detectRange = 5f;
+    public float detectRange = 10f;
+    public int damageAmount = 10;
 
     private Transform playerTransform;
     private Rigidbody2D rb;
@@ -24,7 +25,24 @@ public class EnemyMovement : MonoBehaviour
         {
             Vector2 direction = (playerTransform.position - transform.position).normalized;
             rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+            Debug.Log("detected");
+
+
+            // Check if the enemy is touching the player
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 4f);
+            foreach (Collider2D collider in colliders)
+            {
+                if (collider.gameObject.tag == "Player")
+                {
+                    // Damage the player
+                    Debug.Log("Damage!");
+                    // collider.gameObject.GetComponent<PlayerHealth>().TakeDamage(damageAmount);
+                }
+            }
         }
     }
 }
 
+
+
+            
