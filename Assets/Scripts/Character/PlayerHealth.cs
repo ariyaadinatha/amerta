34c3594abc2a9e88ac0963f2641781c485b41029
Ariya Adinatha <ariyaadinatha21@gmail.com>
2023-05-15 06:26:39 +0700

    healthbar

--- Assets/Scripts/Character/PlayerHealth.cs
+++ Assets/Scripts/Character/PlayerHealth.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int startingHealth = 100;
     private int currentHealth;
+    private float barHealth;
+    public Image healthBar;
 
     void Start()
     {
         currentHealth = startingHealth;
     }
 
+    void Update()
+    {
+        barHealth = (float) currentHealth / startingHealth;
+        healthBar.fillAmount = barHealth;
+        Debug.Log("barhealth: " + barHealth);
+    }
+
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
+        Debug.Log("Player taking damage: " + currentHealth);
         if (currentHealth <= 0)
         {
             // The player has died
