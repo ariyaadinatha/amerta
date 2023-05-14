b2694a89f47b4d8246152b8167d05638bfd2105e
kiipliwooke <kiipliwooke@gmail.com>
2023-05-15 06:44:05 +0700

    add animation cutscene 1

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -7,8 +7,18 @@ public class CameraController : MonoBehaviour
     public GameObject followedObject;
     public Vector3 offset = new Vector3(0, -10, -3);
 
+    private PlayerMovement playerMov;
+
+    private void Awake()
+    {
+        playerMov = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>();
+    }
+
     private void LateUpdate()
     {
-        transform.position = followedObject.transform.position + offset;
+        if (!playerMov.InDialogue() && !playerMov.InTimeline())
+        {
+            transform.position = followedObject.transform.position + offset;
+        }
     }
 }
